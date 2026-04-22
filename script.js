c4fda05b08a7c21a5dbbe5761a6fd0c4d7d40f50
Tammy Chu Shien Ho <130595922+Tamxh@users.noreply.github.com>
2026-04-22 19:36:40 +0200

    a and b done

---
+++ script.js
@@ -0,0 +1,34 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    if (y === 0) {
+        throw new Error("Cannot divide by zero");
+    }
+    return x / y;
+}
+
+function operate(operator, x, y) {
+    switch (operator) {
+        case '+':
+            return add(x, y);
+        case '-':
+            return subtract(x, y);
+        case '*':
+            return multiply(x, y);
+        case '/':
+            return divide(x, y);
+        default:
+            throw new Error("Invalid operator");
+    }   
+}
+
